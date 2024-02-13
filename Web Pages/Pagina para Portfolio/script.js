bdcb98956536a8fdb070474749369be021b5c6ad
Kevitachi <kevinrodriguezbecerra@hotmail.com>
2024-02-13 10:21:30 -0300

    se agrega un JavaScript sin absolutamente nada relevante
    se borran  comentarios y se agrega un archivo de JS, ya se envio la webpage y no se si voy a seguir haciendola pq no se me ocurre que contar en la misma, sadge

---
+++ Web Pages/Pagina para Portfolio/script.js
@@ -0,0 +1,51 @@
+
+let XD1= 'Aguante'
+let XD2= 'Cho'
+let XD2_1="'Gath"
+let XD3= XD1 + " " + XD2 + XD2_1
+
+console.log(XD3)
+
+let a = 1;
+let b = 2;
+let c = a+b;
+
+function MileiSi_o_No(c){
+
+
+if(c=3){
+    return false;
+}
+    return true;
+}
+console.log(MileiSi_o_No(c))
+
+
+//Estoy escribiendo cualquier cosa
+//Objeto
+let name;
+let age;
+let debt;
+
+let pepin={
+    name:'Pepin',
+    age:60,
+    debt: 6000000000
+
+}
+// console.log(pepin)
+
+let ArrayPepin = [pepin, 'Pepe','Pepote','Zanopia'];
+ArrayPepin.push('Espinoza');
+
+console.log(ArrayPepin[0])
+console.log(ArrayPepin[1])
+console.log(ArrayPepin[2])
+console.log(ArrayPepin[3])
+console.log(ArrayPepin[4])
+
+let slice=XD3.slice(7, 16);
+
+
+console.log("Esto es un slice:" +slice)
+

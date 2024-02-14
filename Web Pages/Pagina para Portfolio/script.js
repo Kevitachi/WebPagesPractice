a0f790416c3fe76db2079513f1559c3e12eff5c3
Kevitachi <kevinrodriguezbecerra@hotmail.com>
2024-02-14 12:22:57 -0300

    dump
    xd

--- Web Pages/Pagina para Portfolio/script.js
+++ Web Pages/Pagina para Portfolio/script.js
@@ -6,12 +6,34 @@ let XD3= XD1 + " " + XD2 + XD2_1
 
 console.log(XD3)
 
-let a = 1;
-let b = 2;
-let c = a+b;
+i=1;
+    while(i<=10){
+        i++
+        console.log("ValorWhile:"+i)
+        //Hacete un fibonacci flaco no seas pete
+    }
 
+    do{
+        console.log("Valor DoWhile:"+i)
+        i++
+    }while(i>12)
+
+
+
+
+
+
+    function sum(x,y){
+
+        return x+y;
+    }
+    let plus=sum(10,30);
+    console.log("Resultado de la funcion suma:"+ plus)
+
+
+    
+c=3;
 function MileiSi_o_No(c){
-
 
 if(c=3){
     return false;
@@ -46,6 +68,38 @@ console.log(ArrayPepin[4])
 
 let slice=XD3.slice(7, 16);
 
-
 console.log("Esto es un slice:" +slice)
+
+
+var value= parseInt(prompt("Ingresar un valor entre 1 y 3"))
+
+
+switch(value){
+
+case 1:
+    console.log("Valor 1")
+    break;
+
+case 2:
+    console.log("Valor 2")
+    break;
+
+case  3:
+    console.log("Valor 3")
+    break;
+
+   default: 
+    console.log("Exit")
+    break;
+
+}
+
+for(let i =0; i<=10;i++){
+    let XD1= 'Aguante'
+    let XD2= 'Cho'
+    let XD2_1="'Gath"
+    let XD3= XD1 + " " + XD2 + XD2_1
+    
+    console.log(XD3)
+}
 
